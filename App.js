c9ba758cd1461189a469c9a82a7d8d780b7fa244
Kevin Williams <kwilliams@discounttire.com>
2018-07-23 09:23:36 -0700

    Switched to vanilla react-native

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import { AppFontLoader, GA } from './src/utils';
-import { NativeRouter, Route } from 'react-router-native';
-import { ScreenHit } from 'expo-analytics';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-import Main from './src/containers/Main';
-import reducers from './src/reducers';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-export default class App extends React.Component {
-  componentDidMount() {
-    GA.hit(new ScreenHit('EA-Main'));
-  }
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
+type Props = {};
+export default class App extends Component<Props> {
   render() {
     return (
-      <AppFontLoader>
-        <Provider
-          store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}
-        >
-          <NativeRouter>
-            <Route component={Main} />
-          </NativeRouter>
-        </Provider>
-      </AppFontLoader>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
     );
   }
 }
@@ -32,8 +32,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,5 @@
 import { Dimensions } from 'react-native';
-import { Analytics } from 'expo-analytics';
-
-export * from './AppFontLoader';
 export * from './icons';
 
 export const SCREEN_WIDTH = Dimensions.get('window').width;
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
-
-// TODO Change these IDs
-export const AD_MOB_ID = 'ca-app-pub-6513320241703770/6458258805';
-export const GA = new Analytics('UA-117920354-1');
