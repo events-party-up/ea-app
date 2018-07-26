101317ec6e564a81722307c1cc8a2f60739ca6b2
Kevin Williams <kwilliams@discounttire.com>
2018-07-26 08:55:24 -0700

    Basic calendar rendering with marked days

---
+++ mock/events.js
@@ -0,0 +1,16 @@
+export const EVENTS_BASIC = [
+  {
+    id: '1',
+    title: 'International Eats - Ukraine',
+    date: '2018-07-01T18:30:00.000Z',
+    imageUrl:
+      'https://singles.eventsandadventures.com/images/calendar/4th%20of%20July-2016-10-15-12-04-40-MeggieRoberts.jpg'
+  },
+  {
+    id: '2',
+    title: 'Just for Fun Sand Volleyball (West Valley)',
+    date: '2018-07-02T19:00:00.000Z',
+    imageUrl:
+      'https://singles.eventsandadventures.com/images/dallas/4th%20of%20July-2015-04-13-03-26-56-MarybethWillcox.jpg'
+  }
+];
